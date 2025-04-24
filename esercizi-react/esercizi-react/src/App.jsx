eecf4d6d8ce7f3c4157b19dfbf83f1fd9bb72342
mirko prisciano <mirkoprisciano@gmail.com>
2025-04-24 17:52:22 +0200

    decrementa contatore

--- esercizi-react/esercizi-react/src/App.jsx
+++ esercizi-react/esercizi-react/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
 }
 
 
-export default App */
+export default App 
 
 //esercizio 3//
 
@@ -31,6 +31,37 @@ const App = () => {
   return (
     <div>
       <button onClick={handleClickButton}>Incrementa</button>
+      <p>
+        Counter: {counter} 
+      </p>
+    </div>
+  );
+};
+
+export default App;
+*/
+
+//esercizio 4//
+
+import { useState } from 'react';
+import React from 'react';
+
+const App = () => {
+  const [counter, setCounter] = useState(0); 
+
+  const handleClickButton = () => {
+    setCounter((_counter) => _counter + 1); 
+  };
+
+  const LessClickButton = () => {
+    setCounter((_counter) => _counter - 1); 
+  };
+
+  return (
+    <div>
+      <button onClick={handleClickButton}>Incrementa</button>
+      <button onClick={LessClickButton}>decrementa</button>
+      
       <p>
         Counter: {counter} 
       </p>
