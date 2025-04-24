f288a2f53308319cc967a607fe0a5e4f1ccb00ca
mirko prisciano <mirkoprisciano@gmail.com>
2025-04-24 17:46:07 +0200

    incrementa contatore

--- esercizi-react/esercizi-react/src/App.jsx
+++ esercizi-react/esercizi-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+/* import { useState } from 'react';
 import React from 'react';
 
 const App = () => {
@@ -14,4 +14,28 @@ const App = () => {
 }
 
 
-export default App
+export default App */
+
+//esercizio 3//
+
+import { useState } from 'react';
+import React from 'react';
+
+const App = () => {
+  const [counter, setCounter] = useState(0); 
+
+  const handleClickButton = () => {
+    setCounter((_counter) => _counter + 1); 
+  };
+
+  return (
+    <div>
+      <button onClick={handleClickButton}>Incrementa</button>
+      <p>
+        Counter: {counter} 
+      </p>
+    </div>
+  );
+};
+
+export default App;
