6c1fbdba1375e298b0580684cac3691177407203
mirko prisciano <mirkoprisciano@gmail.com>
2025-05-07 16:55:43 +0200

    usememo

--- esercizi-react/esercizi-react/src/Components/TodoList.jsx
+++ esercizi-react/esercizi-react/src/Components/TodoList.jsx
@@ -27,7 +27,7 @@ const TodoList = () => {
 };
 
 export default TodoList;
-*/
+
 
 import { useState, useCallback } from "react";
 import useFetch from "../Hooks/UseFetch";
@@ -63,4 +63,39 @@ const TodoList = () => {
 };
 
 export default TodoList;
+*/
 
+import { useState, useCallback } from "react";
+import useFetch from "../Hooks/UseFetch";
+import useFilteredTodos from "../Hooks/useFilteredTodos"; 
+
+const TodoList = () => {
+  const { data, loading, error } = useFetch("https://jsonplaceholder.typicode.com/todos");
+  
+  const [search, setSearch] = useState("");
+
+  const handleSearchChange = useCallback((event) => {
+    setSearch(event.target.value);
+  }, []);
+
+  const filteredTodos = useMemo(() => useFilteredTodos(data, search), [data, search]);
+
+  if (loading) return <p>Caricamento...</p>;
+  if (error) return <p>Errore: {error}</p>;
+
+  return (
+    <div>
+      <h2>Lista To-Do</h2>
+      <input onChange={handleSearchChange} type="text" value={search} placeholder="Cerca..." />
+      <ul>
+        {filteredTodos && filteredTodos.map((todo) => (
+          <li key={todo.id}>
+            {todo.title} {todo.completed ? "✅" : "❌"}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
