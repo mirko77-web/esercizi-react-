9931e8af41a247119be063578f4ebc72fca0ea85
mirko prisciano <mirkoprisciano@gmail.com>
2025-05-24 20:16:34 +0200

    aggiungere una funzione per aggiungere un todo

--- todo-list/src/index.ts
+++ todo-list/src/index.ts
@@ -5,3 +5,14 @@ import { Todo } from './types';
 
 const todos: Todo[] = [];
 
+//esercizio 5 
+function addTodo(title: string): Todo {
+  const newTodo: Todo = {
+    id: todos.length > 0 ? todos[todos.length - 1].id + 1 : 1, 
+    title,
+    completed: false
+  };
+
+  todos.push(newTodo);
+  return newTodo;
+}
