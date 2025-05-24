73060a7042f207ad229c5b1e46e7f4e7a82274ea
mirko prisciano <mirkoprisciano@gmail.com>
2025-05-24 20:13:45 +0200

    implementare l'interfaccia todo

--- todo-list/src/index.ts
+++ todo-list/src/index.ts
@@ -1,2 +1,7 @@
 const myName: string = "luigi";
 console.log(myName);
+
+import { Todo } from './types';
+
+const todos: Todo[] = [];
+
--- todo-list/src/types.ts
+++ todo-list/src/types.ts
@@ -1,3 +1,5 @@
+
+
 export interface Todo {
   id: number;
   title: string;
