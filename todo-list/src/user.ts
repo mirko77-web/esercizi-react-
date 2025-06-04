dd5191b723fc77bc1ec8f681544c9e95f5c3a561
mirko prisciano <mirkoprisciano@gmail.com>
2025-06-04 18:13:26 +0200

    metodi della classe user

--- todo-list/src/user.ts
+++ todo-list/src/user.ts
@@ -1,5 +1,4 @@
-
-
+/*
 export class User {
   id: number;
   name: string;
@@ -9,5 +8,26 @@ export class User {
     this.id = id;
     this.name = name;
     this.email = email;
+  }
+}*/
+
+//eserciziom 19
+
+import { Todo } from './types';
+
+export class User {
+  id: number;
+  name: string;
+  email?: string;
+  todos: Todo[] = [];
+
+  constructor(id: number, name: string, email?: string) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+  }
+
+  addTodo(todo: Todo): void {
+    this.todos.push(todo);
   }
 }
